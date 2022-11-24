2d3b4e56bf5819960068e2cbd80b3475c2270403
HaiDang <dangtranhai628@gmail.com>
2022-11-24 20:43:07 +0700

    UPDATE - Add calendar from new package

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,5 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import EventCalendar from 'react-awesome-calendar';
+// import Calendar2 from 'react-event-calendar';
+
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import '@fullcalendar/core/main.css';
+import '@fullcalendar/daygrid/main.css';
+import '@fullcalendar/timegrid/main.css';
+import events3 from './events';
+
 import { RoomPopup } from '../components';
 import { getSpecificRoom } from '../utils/productAPI';
 
@@ -33,10 +42,41 @@ const Calendar = () => {
   };
 
   const handleClose = () => setPopup({});
+
+  const events2 = [
+    {
+      start: '2015-07-20',
+      end: '2015-07-02',
+      eventClasses: 'optionalEvent',
+      title: 'test event',
+      description: 'This is a test description of an event',
+    },
+    {
+      start: '2015-07-19',
+      end: '2015-07-25',
+      title: 'test event',
+      description: 'This is a test description of an event',
+      data: 'you can add what ever random data you may want to use later',
+    },
+  ];
   return (
     <div>
       <div className="px-[200px]">
-        <EventCalendar />
+        {/* <EventCalendar /> */}
+        {/* <Calendar2 month={7} year={2015} events={events2} /> */}
+        <FullCalendar
+          defaultView="dayGridMonth"
+          firstDay={1}
+          locale="es"
+          header={{
+            left: 'prev,next',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay',
+          }}
+          themeSystem="Simplex"
+          plugins={[dayGridPlugin]}
+          events={events3}
+        />
       </div>
       {/* {Object.keys(popup).length !== 0 && <RoomPopup room={popup} handleClose={handleClose} />} */}
     </div>
